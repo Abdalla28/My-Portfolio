5e87415853e3eead2a93e54f872ee4561cd21427
Abdalla28 <109187325+Abdalla28@users.noreply.github.com>
2025-07-17 22:21:39 +0300

    fgfb

--- src/App.js
+++ src/App.js
@@ -35,6 +35,9 @@ function App() {
         <Contact />
         <ScrollToTop />
         <Footer />
+
+        <Analytics />
+        <SpeedInsights />
       </motion.div>
     </AnimatePresence>
   );
